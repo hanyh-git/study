89d44771a8536bb36b53e6f1c9abcc2b55c7f47e
hanyh <han_yahao@163.com>
2023-05-03 00:12:14 +0800

    添加依赖项

--- src/main/java/com/hanyh/study/StudyApplication.java
+++ src/main/java/com/hanyh/study/StudyApplication.java
@@ -2,12 +2,21 @@ package com.hanyh.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyApplication.class, args);
 	}
+
+	@GetMapping("/test")
+	public String test() {
+		return "success";
+	}
+
 
 }
